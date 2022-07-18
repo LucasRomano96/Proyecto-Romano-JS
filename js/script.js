9bae8330a99bdba7c0acbf43adad02a81a7420d4
Lucas <lucasromano96@gmail.com>
2022-07-18 10:54:10 -0300

    Desafio entregable

--- js/script.js
+++ js/script.js
@@ -1,19 +1,60 @@
-const color = 'Negro';
+const savedPIN = '7777'
+let ingresar = false
 
-for(let i = 2; i >= 0; i--){
-    let opcion = prompt('Ingrese un color. Tenes '+ (i+1) + ' intentos.');
-    if(opcion == color){
-        alert('Ganaste');
+for (let i = 2; i >= 0; i--) {
+    let userPIN = prompt('Ingresa tu pin. Tenés ' + (i + 1) + ' intentos.');
+    if (userPIN === savedPIN) {
+        alert('Login exitoso');
+        ingresar = true;
         break;
-    } else{
-        opcion = prompt('Perdiste, te quedan '+ i);
+    } else {
+        alert('Error. Te quedan ' + i + ' intentos.');
     }
-    
+
 }
 
+function menu(){
+    if (ingresar) {
+        let saldo = 5000;
+        
+        let opcion = prompt('Elegí una opción: \n1- Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \nPresioná X para finalizar.');
+        while (opcion != 'X' && opcion != 'x') {
+    
+            switch (opcion) {
+                case '1':
+                    alert('Tu saldo es $ ' + saldo);
+                    break;
+                case '2':
+                    let retiro = parseInt(prompt('Ingresa cantidad a retirar'));
+                    if (retiro <= saldo) {
+                        saldo-=retiro;
+                        alert('Retiro exitoso. Tu nuevo saldo es $ ' + saldo);
+                    } else {
+                        alert('Fondos insuficientes');
+                    }
+                    break;
+                case '3':
+                    let deposito = parseInt(prompt('Ingresa monto a depositar'));
+                    saldo += deposito;
+                    alert('Depósito exitoso. Tu nuevo saldo es $ ' + saldo);
+                    break;
+                default:
+                    alert('Elegiste una opción inválida');
+                    break;
+            }
+    
+            opcion = prompt('Elegí una opción: \n1- Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \n Presioná X para finalizar.'); 
+        }
+    
+    
+    } else {
+        alert('Retendremos tu tarjeta por seguridad');
+    }
+    
+    alert('Adiós');
+}
 
-
-
+menu();
 
 
 
