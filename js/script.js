6cc82866ab0565d060f456bc7ebd5795fb64447e
Lucas <lucasromano96@gmail.com>
2022-07-26 15:54:42 -0300

    Simulador de turnos

--- js/script.js
+++ js/script.js
@@ -1,60 +1,33 @@
-const savedPIN = '7777'
-let ingresar = false
+const mensaje = alert('Bienvendidas, los dias y horarios del salon de belleza son de Lunes a Sabado, desde las 9hs hasta 21hs.');
+const mensaje2 = alert('Para pedir un turno ingrese dia, horario, nombre y apellido por favor.');
 
-for (let i = 2; i >= 0; i--) {
-    let userPIN = prompt('Ingresa tu pin. Tenés ' + (i + 1) + ' intentos.');
-    if (userPIN === savedPIN) {
-        alert('Login exitoso');
-        ingresar = true;
-        break;
-    } else {
-        alert('Error. Te quedan ' + i + ' intentos.');
-    }
-
+function turnos(dia, horario, nombre, apellido){
+    this.dia = dia;
+    this.horario = horario;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    
 }
 
-function menu(){
-    if (ingresar) {
-        let saldo = 5000;
-        
-        let opcion = prompt('Elegí una opción: \n1- Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \nPresioná X para finalizar.');
-        while (opcion != 'X' && opcion != 'x') {
-    
-            switch (opcion) {
-                case '1':
-                    alert('Tu saldo es $ ' + saldo);
-                    break;
-                case '2':
-                    let retiro = parseInt(prompt('Ingresa cantidad a retirar'));
-                    if (retiro <= saldo) {
-                        saldo-=retiro;
-                        alert('Retiro exitoso. Tu nuevo saldo es $ ' + saldo);
-                    } else {
-                        alert('Fondos insuficientes');
-                    }
-                    break;
-                case '3':
-                    let deposito = parseInt(prompt('Ingresa monto a depositar'));
-                    saldo += deposito;
-                    alert('Depósito exitoso. Tu nuevo saldo es $ ' + saldo);
-                    break;
-                default:
-                    alert('Elegiste una opción inválida');
-                    break;
-            }
-    
-            opcion = prompt('Elegí una opción: \n1- Saldo. \n2 - Retiro de dinero. \n3 - Depósito. \n Presioná X para finalizar.'); 
-        }
-    
-    
-    } else {
-        alert('Retendremos tu tarjeta por seguridad');
-    }
-    
-    alert('Adiós');
-}
+const horariosInvalidos = [21.15, 21.30, 21.45, 22.00, 22.15, 22.30, 22.45, 23.00, 23.15, 23.30, 23.45, 1.00, 1.15, 1.30, 1.45, 2.00, 2.15, 2.30, 2.45, 3.00, 3.15, 3.30, 3.45, 4.00, 4.15, 4.30, 4.45, 5.00, 5.15, 5.30, 5.45, 6.00, 6.15, 6.30, 6.45, 7.00, 7.15, 7.30, 7.45];
+console.log(horariosInvalidos);
+horariosInvalidos.push(8.00, 8.15, 8.30, 8.45);
+console.log(horariosInvalidos);
 
-menu();
+function horariosOut(){
+    if(horarioTurnos == horariosInvalidos){
+        alert('Esa hora esta fuera de nuestro horario de atencion');
+    }
+}
+let diaTurnos = prompt('Ingrese un dia');
+let horarioTurnos = prompt('Ingrese en que horario');
+let nombreTurnos = prompt('Ingrese su nombre');
+let apellidoTurnos = prompt('Ingrese su apellido');
+
+const turnoUsuario = new turnos(diaTurnos, horarioTurnos, nombreTurnos, apellidoTurnos);
+
+console.log(turnoUsuario);
+
 
 
 
