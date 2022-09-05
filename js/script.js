0508a3c96924a0313852c82bf556886442487a01
Lucas <lucasromano96@gmail.com>
2022-09-05 15:10:55 -0300

    Desafio Final

--- js/script.js
+++ js/script.js
@@ -1,33 +1,4 @@
-const mensaje = Swal.fire(
-  "Bienvenidas, solicite un turno de lunes a sabados dentro de las 9hs y 20hs",
-  "You clicked the button!",
-  "success"
-);
-const mensaje2 = Swal.fire(
-  "Para pedir un turno ingrese dia, horario, nombre, apellido y email por favor",
-  "You clicked the button!",
-  "success"
-);
-
-function turnos(dia, horario, nombre, apellido) {
-  this.dia = dia;
-  this.horario = horario;
-  this.nombre = nombre;
-  this.apellido = apellido;
-}
-
-const horariosInvalidos = [
-  21.15, 21.3, 21.45, 22.0, 22.15, 22.3, 22.45, 23.0, 23.15, 23.3, 23.45, 1.0,
-  1.15, 1.3, 1.45, 2.0, 2.15, 2.3, 2.45, 3.0, 3.15, 3.3, 3.45, 4.0, 4.15, 4.3,
-  4.45, 5.0, 5.15, 5.3, 5.45, 6.0, 6.15, 6.3, 6.45, 7.0, 7.15, 7.3, 7.45,
-];
-
-console.log(horariosInvalidos);
-horariosInvalidos.push(8.0, 8.15, 8.3, 8.45);
-console.log(horariosInvalidos);
-
-let diaInvalido = "Domingo";
-
+let usuario;
 
 let nuevoMensaje = document.createElement("h2");
 nuevoMensaje.innerHTML = "<h2>Gracias por elegirnos!</h2>";
@@ -68,31 +39,31 @@ class Usuario{
     this.dia = dia;
   }
 }
-/* let usuario = new Usuario(btnApellido.value, btnEmail.value, btnHorario.value, btnNombre.value, btnDia.value);
- */
-/* function formularioData() {
-  if (btnApellido.value != "") {
-    usuario.push(btnApellido.value)
-    console.log(usuario[btnApellido].value);
-  }
-}
-let usuario = []; */
-/* usuario.push(btnApellido.value, btnEmail.value, btnHorario.value, btnNombre.value, btnDia.value); */
 
 const reserva = document.getElementById("formularioPrincipal");
 reserva.addEventListener("submit", (e) => {
   e.preventDefault();
+  usuario = new Usuario(btnApellido.value, btnEmail.value, btnHorario.value, btnNombre.value, btnDia.value);
+  console.log(usuario);
+  verificarEmail(btnEmail.value);
   if (btnDia.value == diaInvalido) {
     btnDia.value = "";
     alert("Dia no valido, ingrese otro dia.");
-  } else if (btnHorario.value <= 8 || btnHorario.value >= 22) {
+  } 
+  if (btnHorario.value <= 8 || btnHorario.value >= 22) {
     btnHorario.value = "";
     alert("Esa hora esta fuera de nuestro horario de atencion");
-  } else{
-    Swal.fire(`Gracias! Su turno ha sido reservado para el dia ${btnDia.value}, a las ${btnHorario.value}`);
-  }
-  verificarEmail(btnEmail.value);
-  reset();
+  } 
+  if (cambiarEmail(btnEmail.value) == false) {
+    btnEmail.value = "";
+  } 
+  
+  setTimeout(() => {
+    if (btnEmail.value !="") {
+      Swal.fire(`Gracias! Su turno ha sido reservado para el dia ${btnDia.value}, a las ${btnHorario.value}`);
+      reset();
+    }
+  },1700)
 });
 
 
@@ -110,6 +81,49 @@ async function verificarEmail(emailSolicitante){
   const dataJason = await resp.json();
   console.log(dataJason);
 };
+
+function cambiarEmail(emailSolicitante) {
+  fetch(`https://disify.com/api/email/${emailSolicitante}`)
+  .then((response) => response.json())
+  .then ((data) => {
+    if (data.dns == false) {
+      btnEmail.value = "";
+      alert("Ha introducido un email incorrecto");
+      return false;
+    }
+  })
+};
+
+
+const mensaje = Swal.fire(
+  "Bienvenidas, solicite un turno de lunes a sabados dentro de las 9hs y 20hs",
+  "You clicked the button!",
+  "success"
+);
+const mensaje2 = Swal.fire(
+  "Para pedir un turno ingrese dia, horario, nombre, apellido y email por favor",
+  "You clicked the button!",
+  "success"
+);
+
+function turnos(dia, horario, nombre, apellido) {
+  this.dia = dia;
+  this.horario = horario;
+  this.nombre = nombre;
+  this.apellido = apellido;
+}
+
+const horariosInvalidos = [
+  21.15, 21.3, 21.45, 22.0, 22.15, 22.3, 22.45, 23.0, 23.15, 23.3, 23.45, 1.0,
+  1.15, 1.3, 1.45, 2.0, 2.15, 2.3, 2.45, 3.0, 3.15, 3.3, 3.45, 4.0, 4.15, 4.3,
+  4.45, 5.0, 5.15, 5.3, 5.45, 6.0, 6.15, 6.3, 6.45, 7.0, 7.15, 7.3, 7.45,
+];
+
+console.log(horariosInvalidos);
+horariosInvalidos.push(8.0, 8.15, 8.3, 8.45);
+console.log(horariosInvalidos);
+
+let diaInvalido = "Domingo";
 
 
  
