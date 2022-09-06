59cd724923ac629586d54227582575e883589a43
Lucas <lucasromano96@gmail.com>
2022-09-06 15:09:26 -0300

    desafio final 3

--- js/script.js
+++ js/script.js
@@ -49,11 +49,7 @@ reserva.addEventListener("submit", (e) => {
   if (btnDia.value == diaInvalido) {
     btnDia.value = "";
     alert("Dia no valido, ingrese otro dia.");
-  } 
-  if (btnHorario.value <= 8 || btnHorario.value >= 22) {
-    btnHorario.value = "";
-    alert("Esa hora esta fuera de nuestro horario de atencion");
-  } 
+  }
   if (cambiarEmail(btnEmail.value) == false) {
     btnEmail.value = "";
   } 
@@ -122,12 +118,6 @@ function turnos(dia, horario, nombre, apellido) {
   this.nombre = nombre;
   this.apellido = apellido;
 }
-
-const horariosInvalidos = [
-  21.15, 21.3, 21.45, 22.0, 22.15, 22.3, 22.45, 23.0, 23.15, 23.3, 23.45, 1.0,
-  1.15, 1.3, 1.45, 2.0, 2.15, 2.3, 2.45, 3.0, 3.15, 3.3, 3.45, 4.0, 4.15, 4.3,
-  4.45, 5.0, 5.15, 5.3, 5.45, 6.0, 6.15, 6.3, 6.45, 7.0, 7.15, 7.3, 7.45,
-];
 
 console.log(horariosInvalidos);
 horariosInvalidos.push(8.0, 8.15, 8.3, 8.45);
